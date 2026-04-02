4260d97fba4973a502f82e0fd53d6edfd6b8352c
Sfuborisw <borix3739@gmail.com>
2026-04-01 19:31:48 -0700

    fix: update tailwind config to able light/dark mode

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,14 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        poe_dark: "#0f0f0f",
-        poe_gold: "#c8a165",
-        poe_border: "#2a2a2a",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
